af08ac2a5f0912900bfd88843dac5e0599dc1e0f
tulsi <tulsihirpara28@gmail.com>
2026-04-13 15:24:31 +0530

    [TH] add db sync in index.js

--- src/models/index.js
+++ src/models/index.js
@@ -1,1 +1,18 @@
+const connectDatabase = require("../config/dbConfig");
+
+// first load all models before sync
+const users = require("./user");
+
 // db sync
+connectDatabase
+  .sync({ alter: true })
+  .then(() => {
+    console.log("DB sync successfully...");
+  })
+  .catch((error) => {
+    console.log("Error while db sync: ", error);
+  });
+
+exports.module = {
+  users,
+};
